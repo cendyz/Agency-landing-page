534c50d1ad18209a7228e42cbf5d28f16d3ee667
cendyz <arek57823@gmail.com>
2024-11-18 19:52:41 +0100

    Agency landing page

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+
+
+const App = () => {
+return (
+	<>
+	<nav className="nav-mobile">
+		<img src="src/img/logo.svg" alt="Logo sunny side" className="nav-mobile-logo" />
+		<img src="src/img/icon-hamburger.svg" alt="Menu button" className="nav-mobile-btn" />
+	</nav>
+	<header className="header">
+		<h1 className="header-title">we are creatives</h1>
+		<img src="src/img/icon-arrow-down.svg" alt="Arrow down icon" className="header-arrow" />
+		<img src="src/img/mobile/image-header.jpg" alt="Header image" className="header-img"/>
+	</header>
+	</>
+)
+
+};
+export default App;
+
+
+
+
+// About
+//   Services
+//   Projects
+//   Contact
+
+//   We are creatives
+
+//   Transform your brand
+
+//   We are a full-service creative agency specializing in helping brands grow fast. 
+//   Engage your clients through compelling visuals that do most of the marketing for you.
+
+//   Learn more
+
+//   Stand out to the right audience
+
+//   Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. 
+
+//   Learn more
+
+//   Graphic design
+//   Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.
+
+//   Photography
+//   Increase your credibility by getting the most stunning, high-quality photos that improve your business image.
+
+//   Client testimonials
+
+//   We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
+//   Emily R.
+//   Marketing Director
+
+//   Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
+//   Thomas S.
+//   Chief Operating Officer
+
+//   Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+//   Jennie F.
+//   Business Owner
+
+//   About
+//   Services
+//   Projects
+//   Contact
